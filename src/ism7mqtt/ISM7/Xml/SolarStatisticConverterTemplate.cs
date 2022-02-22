77a01658953fa9ea4f174cd3f57391883acda875
zivillian <devel@zivillian.de>
2022-02-22 21:03:15 +0100

    implement SolarStatisticConverterTemplate

--- src/ism7mqtt/ISM7/Xml/SolarStatisticConverterTemplate.cs
+++ src/ism7mqtt/ISM7/Xml/SolarStatisticConverterTemplate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using ism7mqtt.ISM7.Protocol;
 using Newtonsoft.Json.Linq;
 
@@ -7,16 +8,42 @@ namespace ism7mqtt.ISM7.Xml
 {
     public class SolarStatisticConverterTemplate : MultiTelegramConverterTemplateBase
     {
+        private const string KwType = "SolarertragW_kW";
+        private const string KwMwType = "SolarertragW_kW_MW";
+        private readonly List<(ushort, uint)> _values = new();
+
         public override void AddTelegram(ushort telegram, byte low, byte high)
         {
-            Console.Error.WriteLine($"SolarStatisticConverter: T:{telegram} H:{high} L:{low}");
+            if (Type == KwMwType || Type == KwType)
+            {
+                _values.Add((telegram, (uint)(high << 8) | low));
+            }
+            else
+            {
+                Console.Error.WriteLine($"SolarStatisticConverter({Type}): T:{telegram} H:{high} L:{low}");
+            }
         }
 
-        public override bool HasValue => false;
+        public override bool HasValue => Type switch
+        {
+            KwType => _values.Count == 2,
+            KwMwType => _values.Count == 3,
+            _ => false
+        };
 
         public override JValue GetValue()
         {
-            throw new NotImplementedException($"CTID '{CTID}' is not yet implemented");
+            var values = _values.OrderBy(x => x.Item1).ToList();
+            var wh = values[0].Item2;
+            var kwh = values[1].Item2;
+            var value = wh + (1_000UL * kwh);
+            if (Type == KwMwType)
+            {
+                var mwh = values[2].Item2;
+                value += mwh * 1_000_000UL;
+            }
+            _values.Clear();
+            return new JValue(value);
         }
 
         public override IEnumerable<InfoWrite> GetWrite(JValue value)
