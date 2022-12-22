537e855384c0328c98f2d0b812ddf3f088f23364
Adrian Batzill <dev@batzill.com>
2022-12-22 15:27:56 +0100

    Also pass through OtherParameterDescriptors to HA discovery. Writing to them will fail though, if not yet implemented by ism7mqtt

--- src/ism7mqtt/ISM7/Xml/OtherParameterDescriptor.cs
+++ src/ism7mqtt/ISM7/Xml/OtherParameterDescriptor.cs
@@ -10,6 +10,6 @@ namespace ism7mqtt.ISM7.Xml
         [XmlElement("Decimals")]
         public int Decimals { get; set; }
 
-        public override string HomeAssistantType => null;
+        public override string HomeAssistantType => IsWritable ? "text" : "sensor";
     }
 }
