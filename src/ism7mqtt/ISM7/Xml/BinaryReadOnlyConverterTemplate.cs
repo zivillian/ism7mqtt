f8e555db5e31a70aefeaca603a4957a7af72e609
zivillian <devel@zivillian.de>
2023-05-15 22:43:07 +0200

    fix output of BinaryReadOnlyConverterTemplate and corresponding ha discovery topics
    the parameter & converter templates clearly show that the output of the
    BinaryReadOnlyConverterTemplate needs to be numeric (0,1) to be used
    as input for ListParameterDescriptor KeyValueList mapping or as input
    for NumericParameterDescriptor

--- src/ism7mqtt/ISM7/Xml/BinaryReadOnlyConverterTemplate.cs
+++ src/ism7mqtt/ISM7/Xml/BinaryReadOnlyConverterTemplate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Nodes;
 using System.Xml.Serialization;
 using ism7mqtt.ISM7.Protocol;
@@ -22,13 +23,14 @@ namespace ism7mqtt.ISM7.Xml
             _bit = (value >> Bitnumber) & 0x1;
         }
 
+        [MemberNotNullWhen(true, nameof(_bit))]
         public override bool HasValue => _bit.HasValue;
 
         public override JsonValue GetValue()
         {
             if (!HasValue)
                 throw new InvalidOperationException();
-            var result = JsonValue.Create(_bit.Value == OnValue);
+            var result = JsonValue.Create(_bit.Value == OnValue ? 1 : 0);
             _bit = null;
             return result;
         }
