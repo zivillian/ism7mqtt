a90d46a69c646e2bf3bd6414f2670578ece1f213
allcoolusernamesaregone <deraills@gmail.com>
2024-08-21 10:05:28 +0200

    Use XmlSerializerNamespaces to remove the namespace attributes

--- src/ism7mqtt/ISM7/Protocol/XmlPayload.cs
+++ src/ism7mqtt/ISM7/Protocol/XmlPayload.cs
@@ -10,14 +10,18 @@ namespace ism7mqtt.ISM7.Protocol
     public abstract class XmlPayload : IPayload
     {
         private static readonly ConcurrentDictionary<Type, XmlSerializer> _serializers = new ConcurrentDictionary<Type, XmlSerializer>();
+        private static readonly XmlSerializerNamespaces _serializerNamespaces = new XmlSerializerNamespaces();
 
+        static XmlPayload() {
+            _serializerNamespaces.Add("", "");
+        }
         public byte[] Serialize()
         {
             using var sw = new Utf8StringWriter();
             var xmlWriter = XmlWriter.Create(sw, new XmlWriterSettings {Indent = false});
 
             var serializer = _serializers.GetOrAdd(GetType(), x => new XmlSerializer(x));
-            serializer.Serialize(xmlWriter, this);
+            serializer.Serialize(xmlWriter, this, _serializerNamespaces);
             sw.Flush();
             var xml = sw.ToString();
             return Encoding.UTF8.GetBytes(xml);
